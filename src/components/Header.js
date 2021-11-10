13016c1715cea1a030023033c5c42a30d9111888
Victor Araujo <victor@lunenetworks.com>
2021-11-10 12:37:28 -0800

    header using vh

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,3 @@
-
 /**
  * Common header template to be used for separate pages.
  */
@@ -9,34 +8,24 @@ const styles = () => ({
   container: {
     display: 'flex',
     flexDirection: 'column',
-    height: '50px',
+    height: '5vh',
   },
   header: {
     margin: 'auto',
   },
-  logo: {
+  logoDiv: {
     alignItems: 'center',
     display: 'inline-flex',
     height: '100%',
   },
+  logo: {
+    height: '4vh',
+  },
   logoLink: {
-    marginLeft: '8px',
+    display: 'flex',
+    marginLeft: '1vh',
     textDecoration: 'none',
   },
-  // tab: {
-  //   color: 'white',
-  //   cursor: 'pointer',
-  //   display: 'flex',
-  //   flexGrow: '1',
-  //   justifyContent: 'center',
-  //   listStyle: 'none',
-  //   padding: '6px 12px',
-  //   textDecoration: 'none !important',
-  //   userSelect: 'none',
-  //   '&:hover': {
-  //     backgroundColor: '#222222',
-  //   }
-  // },
   tabs: {
     backgroundColor: '#434343',
     color: 'white',
@@ -44,13 +33,12 @@ const styles = () => ({
   },
   title: {
     color: 'white',
-    margin: '0 8px',
-    padding: '4px 0',
+    fontSize: '4vh',
+    margin: '0 1vh',
     textAlign: 'center',
   }
 });
 
-/** Height set to 6% */
 class Header extends Component {
   render() {
     const { classes } = this.props;
@@ -59,8 +47,8 @@ class Header extends Component {
       <div className={classes.container}>
         <div className={classes.tabs}>
           <a href='/' className={classes.logoLink}>
-            <div className={classes.logo}>
-              <img height={36} src='./images/logo.png' alt='Trender logo' />
+            <div className={classes.logoDiv}>
+              <img className={classes.logo} src='./images/logo.png' alt='Trender logo' />
               <Typography className={classes.title} variant='h4'>
                 Trender
               </Typography>
