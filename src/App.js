35c97cb45a321d7ce88339af5cfc58eaedaa34e5
Victor Araujo <victor@orange.corp.lunenetworks.com>
2021-11-28 23:06:21 -0800

    broswer router config

--- src/App.js
+++ src/App.js
@@ -26,7 +26,7 @@ class App extends Component {
     return (
       <>
         <Header />
-        <BrowserRouter >
+        <BrowserRouter basename={process.env.PUBLIC_URL} >
           {/* A <Switch> looks through all its children <Route> elements and
           renders the first one whose path matches the current URL. Use a
           <Switch> any time you have multiple routes, but you want only one of
